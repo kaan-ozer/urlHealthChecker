280bbffdd6db0d8aec4fa80dac1b333c7e73dbcd
kaan-ozer <kaanozer35@gmail.com>
2026-05-08 22:31:19 +0300

    initial project setup from spring initializr

---
+++ src/main/java/com/example/urlHealthChecker/UrlHealthCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.urlHealthChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlHealthCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlHealthCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/urlHealthChecker/UrlHealthCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlHealthChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlHealthCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
